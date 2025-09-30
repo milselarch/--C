5a8d2b50fce4df87e540db9f5a6e6852343e06a2
Charles <milselarch@protonmail.com>
2025-09-30 16:18:36 +0800

    feat: add tacky binary op support

--- src/tacky/tacky_symbols.rs
+++ src/tacky/tacky_symbols.rs
@@ -1,8 +1,8 @@
 use std::fmt::format;
 use std::hash::{Hash, Hasher};
 use crate::parser::parse::{
-    Identifier, ASTProgram, SupportedUnaryOperators, ASTFunction,
-    ExpressionVariant, ASTConstant, parse_from_filepath
+    Identifier, ASTProgram, SupportedUnaryOperators, ASTFunction, ExpressionVariant,
+    ASTConstant, parse_from_filepath, SupportedBinaryOperators
 };
 use crate::parser::parser_helpers::{ParseError, PoppedTokenContext};
 
@@ -28,14 +28,41 @@ impl Hash for TackyVariable {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct UnrollResult {
+    pub instructions: Vec<TackyInstruction>,
+    pub value: TackyValue,
+    pub next_free_var_id: u64
+}
+impl UnrollResult {
+    pub fn new(
+        instructions: Vec<TackyInstruction>,
+        value: TackyValue,
+        next_free_var_id: u64
+    ) -> UnrollResult {
+        UnrollResult {
+            instructions,
+            value,
+            next_free_var_id
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum TackyValue {
     Constant(ASTConstant),
     Var(TackyVariable)
 }
+impl TackyValue {
+    pub fn get_id(&self) -> Option<u64> {
+        match self {
+            TackyValue::Constant(_) => None,
+            TackyValue::Var(v) => Some(v.id)
+        }
+    }
+}
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct UnaryInstruction {
     pub operator: SupportedUnaryOperators,
     pub src: TackyValue,
@@ -48,42 +75,96 @@ impl UnaryInstruction {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
+pub struct BinaryInstruction {
+    pub operator: SupportedBinaryOperators,
+    pub left: TackyValue,
+    pub right: TackyValue,
+    pub dst: TackyVariable,
+    pub pop_context: Option<PoppedTokenContext>
+}
+impl BinaryInstruction {
+    pub fn to_tacky_instruction(&self) -> TackyInstruction {
+        TackyInstruction::BinaryInstruction(self.clone())
+    }
+}
+
+#[derive(Clone, Debug)]
 pub enum TackyInstruction {
     UnaryInstruction(UnaryInstruction),
+    BinaryInstruction(BinaryInstruction),
     Return(TackyValue),
 }
 impl TackyInstruction {
     pub fn unroll_expression(
         expr_item: ExpressionVariant,
         var_counter: u64
-    ) -> (Vec<TackyInstruction>, TackyValue) {
+    ) -> UnrollResult {
         match expr_item {
             ExpressionVariant::Constant(ast_constant) => {
-                (
+                UnrollResult::new(
                     Vec::new(),
-                    TackyValue::Constant(ast_constant.clone())
+                    TackyValue::Constant(ast_constant.clone()),
+                    var_counter
                 )
             },
             ExpressionVariant::UnaryOperation(
                 operator, sub_expr
             ) => {
-                let new_var = TackyVariable::new(var_counter);
                 let sub_expr_item = sub_expr.expr_item.clone();
-                let (sub_instructions, inner_value) = Self::unroll_expression(
-                    sub_expr_item, var_counter + 1
+                let inner_unroll_res = Self::unroll_expression(
+                    sub_expr_item, var_counter
                 );
+
+                let var_counter = inner_unroll_res.next_free_var_id;
+                let new_var = TackyVariable::new(var_counter);
                 let new_unary_instruction = UnaryInstruction {
                     operator,
-                    src: inner_value,
+                    src: inner_unroll_res.value,
                     dst: new_var.clone(),
                     pop_context: sub_expr.pop_context.clone()
                 };
+
+                let sub_instructions = inner_unroll_res.instructions;
                 let mut instructions = sub_instructions.clone();
                 instructions.push(new_unary_instruction.to_tacky_instruction());
-                (
+
+                UnrollResult::new(
                     instructions,
-                    TackyValue::Var(new_var)
+                    TackyValue::Var(new_var),
+                    var_counter + 1
+                )
+            }
+            ExpressionVariant::BinaryOperation(operator, left, right) => {
+                let left_expr_item = left.expr_item.clone();
+                let right_expr_item = right.expr_item.clone();
+
+                let left_unroll =
+                    Self::unroll_expression(left_expr_item, var_counter);
+                let var_counter = left_unroll.next_free_var_id;
+                let right_unroll =
+                    Self::unroll_expression(right_expr_item, var_counter);
+                let var_counter = left_unroll.next_free_var_id;
+
+                let new_var = TackyVariable::new(var_counter);
+                let new_binary_instruction = BinaryInstruction {
+                    operator,
+                    left: left_unroll.value,
+                    right: right_unroll.value,
+                    dst: new_var.clone(),
+                    pop_context: right.pop_context.clone()
+                };
+
+                let left_instructions = left_unroll.instructions;
+                let right_instructions = right_unroll.instructions;
+                let mut instructions = left_instructions.clone();
+                instructions.extend(right_instructions.clone());
+                instructions.push(new_binary_instruction.to_tacky_instruction());
+
+                UnrollResult::new(
+                    instructions,
+                    TackyValue::Var(new_var),
+                    var_counter + 1
                 )
             }
             ExpressionVariant::ParensWrapped(sub_expr) => {
@@ -108,9 +189,10 @@ impl TackyFunction {
         let statement = &function.body;
         let expression = &statement.expression;
         let expr_item = expression.expr_item.clone();
-        let (mut sub_instructions, temp_value) =
-            TackyInstruction::unroll_expression(expr_item, 0);
+        let inner_unroll = TackyInstruction::unroll_expression(expr_item, 0);
 
+        let temp_value = inner_unroll.value;
+        let mut sub_instructions = inner_unroll.instructions;
         let return_instruction = TackyInstruction::Return(temp_value);
         sub_instructions.push(return_instruction);
 
