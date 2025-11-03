44f9232a8fc6f6801b51a77385226881550a2fe8
Charles <milselarch@protonmail.com>
2025-11-03 17:58:40 +0800

    chore: flush changes

--- src/automata/composer.rs
+++ src/automata/composer.rs
@@ -11,6 +11,8 @@ TODO:
     - APL style rule builders (accumulator, reduct input, unary expansion)
 */
 
+const VOID_STATE: u32 = 0;
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Sequence)]
 pub enum Direction {
     Left,
@@ -36,62 +38,85 @@ pub struct TapeState {
     tape_key: TapeKey,
     tape_cell_state: u32
 }
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum TapeCellState {
-    VOID,
-    HALT,
-    TapeState(TapeState),
-}
-impl Ord for TapeCellState {
-    fn cmp(&self, other: &Self) -> Ordering {
-        use TapeCellState::*;
-        match (self, other) {
-            (VOID, VOID) => Ordering::Equal,
-            (VOID, _) => Ordering::Less,
-            (_, VOID) => Ordering::Greater,
-            (HALT, HALT) => Ordering::Equal,
-            (HALT, _) => Ordering::Greater,
-            (_, HALT) => Ordering::Less,
-            (TapeState(a), TapeState(b)) => a.cmp(b),
+impl TapeState {
+    pub fn new(
+        tape_key: TapeKey,
+        tape_cell_state: u32
+    ) -> TapeState {
+        TapeState {
+            tape_key,
+            tape_cell_state,
         }
-    }
-}
-impl PartialOrd for TapeCellState {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
     }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct CellExpectation {
     direction: Direction,
-    expected_state: TapeCellState,
+    expected_state: TapeState,
 }
-pub fn new_cell_expectation(
+impl CellExpectation {
+    pub fn new(
+        direction: Direction,
+        expected_state: TapeState
+    ) -> CellExpectation {
+        CellExpectation {
+            direction,
+            expected_state,
+        }
+    }
+
+    pub fn to_identifier(&self) -> TapeCellIdentifier {
+        TapeCellIdentifier::new(
+            self.expected_state.tape_key.clone(),
+            self.direction.clone(),
+        )
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct TapeCellIdentifier {
+    tape_key: TapeKey,
     direction: Direction,
-    expected_state: TapeCellState
-) -> CellExpectation {
-    CellExpectation {
-        direction,
-        expected_state,
+}
+impl TapeCellIdentifier {
+    pub fn new(
+        tape_key: TapeKey,
+        direction: Direction
+    ) -> TapeCellIdentifier {
+        TapeCellIdentifier {
+            tape_key,
+            direction,
+        }
     }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct CellExpectationCombo {
-    cell_expectations: HashSet<CellExpectation>,
+    /*
+    Represents the expectation that a bunch of adjacent cells
+    certain corresponding states
+    */
+    cell_expectations: HashMap<TapeCellIdentifier, CellExpectation>
 }
 impl CellExpectationCombo {
     pub fn new(
-        cell_expectations: HashSet<CellExpectation>
+        cell_expectations: HashMap<TapeCellIdentifier, CellExpectation>
     ) -> CellExpectationCombo {
         CellExpectationCombo { cell_expectations }
     }
     pub fn new_empty() -> CellExpectationCombo {
         CellExpectationCombo {
-            cell_expectations: HashSet::new(),
+            cell_expectations: HashMap::new()
         }
+    }
+    pub fn insert_expectation(
+        &mut self, expectation: CellExpectation
+    ) {
+        let identifier = expectation.to_identifier();
+        // ensure no duplicate expectations for same tape cell
+        let prev_value = self.cell_expectations.insert(identifier, expectation);
+        assert_eq!(prev_value, None);
     }
 }
 
@@ -100,15 +125,15 @@ pub struct WriteRule {
     expectations: CellExpectationCombo,
     write_tape: WritableTapeKey,
     // cell transition to apply to own tapes cell
-    self_write_value: TapeCellState,
+    self_write_value: TapeState,
     // cell transitions to apply to other read tape cells
     // at the same position
-    read_tape_write_values: Vec<(ReadableTapeKey, TapeCellState)>,
+    read_tape_write_values: Vec<(ReadableTapeKey, TapeState)>,
 }
 impl Hash for CellExpectationCombo {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         let mut expectations_vec: Vec<&CellExpectation> =
-            self.cell_expectations.iter().collect();
+            self.cell_expectations.values().collect();
         expectations_vec.sort();
         for expectation in expectations_vec {
             expectation.hash(state);
@@ -122,6 +147,8 @@ pub struct Tape {
     // whether cellular automata rules applied to cells on
     // this tape can write to the tape itself
     self_writeable: bool,
+    tape_index: usize,
+
     write_rules: Vec<WriteRule>,
     allowed_states: HashSet<u32>,
     // cells extending rightwards
@@ -133,6 +160,7 @@ impl Tape {
     pub fn new(
         self_writeable: bool,
         write_rules: Vec<WriteRule>,
+        tape_index: usize,
         data: Vec<u32>,
     ) -> Tape {
         if !self_writeable {
@@ -145,9 +173,31 @@ impl Tape {
             self_writeable,
             write_rules,
             allowed_states: Default::default(),
+            tape_index,
             data,
             rev_data: vec![],
         }
+    }
+    pub fn build_cell_expectation(
+        &self, tape_cell_state: u32, direction: Direction
+    ) -> CellExpectation {
+        let tape_state = TapeState::new(
+            if self.self_writeable {
+                TapeKey::Writable(WritableTapeKey {
+                    tape_index: self.tape_index,
+                })
+            } else {
+                TapeKey::Readable(ReadableTapeKey {
+                    tape_index: self.tape_index,
+                })
+            },
+            tape_cell_state,
+        );
+        let cell_expectation = CellExpectation::new(
+            direction,
+            tape_state,
+        );
+        cell_expectation
     }
     pub fn generate_all_combinations(&self) -> HashSet<CellExpectationCombo> {
         /*
@@ -157,11 +207,16 @@ impl Tape {
 
         for direction in enum_iterator::all::<Direction>() {
             let mut combination = CellExpectationCombo::new_empty();
-            for state in &self.allowed_states {
-                todo!()
+            for tape_cell_state in &self.allowed_states {
+                let cell_expectation = self.build_cell_expectation(
+                    *tape_cell_state, direction.clone()
+                );
+                combination.insert_expectation(cell_expectation);
             }
+
+            combinations.insert(combination);
         }
-        todo!()
+        combinations
     }
 }
 
@@ -177,6 +232,23 @@ impl MultiTape {
         read_tapes: Vec<Tape>,
         write_tapes: Vec<Tape>,
     ) -> MultiTape {
+        for (index, tape) in read_tapes.iter().enumerate() {
+            assert_eq!(
+                tape.self_writeable, false,
+                "Read tape at index {} is self-writeable",
+                index
+            );
+            assert_eq!(tape.tape_index, index);
+        }
+        for (index, tape) in write_tapes.iter().enumerate() {
+            assert_eq!(
+                tape.self_writeable, true,
+                "Write tape at index {} is not self-writeable",
+                index
+            );
+            assert_eq!(tape.tape_index, index);
+        }
+
         MultiTape {
             read_tapes,
             write_tapes,
@@ -206,7 +278,7 @@ impl MultiTape {
     pub fn generate_tape_equations(&self) {
         let mut equations = HashMap::new();
         for rule in &self.rules {
-
+            todo!()
         }
     }
 }
